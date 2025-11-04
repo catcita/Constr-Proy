fde18d79d5fd6eb4e8fe9d6310e13e54a3f48b9e
DiegoQuezada7 <dquezada21@alumnos.utalca.cl>
2025-11-04 00:29:02 -0300

    modifiqué el .env.example, pero no sé si todo sirve para verlo en el celu lo mismo y aparezca todo.

--- backend/adoptions-service/src/main/java/com/example/adoptions_service/controller/NotificacionController.java
+++ backend/adoptions-service/src/main/java/com/example/adoptions_service/controller/NotificacionController.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.example.adoptions_service.model.Notificacion;
 import com.example.adoptions_service.repository.NotificacionRepository;
